38776c1ad58140782a7ef92c4b02dd92d66cad25
Edidiong <eddyenin6@gmail.com>
2025-01-06 00:13:10 +0100

    design products viw page

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Flexible Coupon System</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/cart">Cart</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+    </nav>
+
+    {{ $slot }}
+
+<script src="{{ asset('js/script.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <section>
+        <div class="container ">
+            <div class="row">
+                <h2 class="mt-2">Products</h2>
+                <div class="col-md-4 mt-2">
+                    <div class="card p-3">
+                        <div class="card-body">
+                        <h5 class="card-title product-name">Product name</h5>
+                        <p class="card-text product-price"><span>$1000</span></p>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <a href="javascript:void(0)" class="btn btn-sm btn-primary p-2" id="addBtn">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-layout>
