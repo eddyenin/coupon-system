4740e804847890a4b5e183e59a5f6d4e51dee154
Edidiong <eddyenin6@gmail.com>
2025-01-06 00:23:31 +0100

    make product controller and get products from db

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,17 +3,19 @@
         <div class="container ">
             <div class="row">
                 <h2 class="mt-2">Products</h2>
-                <div class="col-md-4 mt-2">
-                    <div class="card p-3">
-                        <div class="card-body">
-                        <h5 class="card-title product-name">Product name</h5>
-                        <p class="card-text product-price"><span>$1000</span></p>
-                        </div>
-                        <div class="d-flex justify-content-end">
-                            <a href="javascript:void(0)" class="btn btn-sm btn-primary p-2" id="addBtn">Add to cart</a>
+                    @foreach ($products as $product)
+                    <div class="col-md-4 mt-2">
+                        <div class="card p-3">
+                            <div class="card-body">
+                            <h5 class="card-title product-name">{{ $product->name }}</h5>
+                            <p class="card-text product-price"><span>${{ $product->price }}</span></p>
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <a href="javascript:void(0)" class="btn btn-sm btn-primary p-2" id="addBtn">Add to cart</a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                     @endforeach
             </div>
         </div>
     </section>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+Route::get('/',[ProductController::class,'index']);
 
